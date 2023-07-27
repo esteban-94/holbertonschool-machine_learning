cbaf9503062f60398e3a378bd1cabb5abc33d756
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-07-26 19:27:26 -0500

    fix definition of equal

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -6,7 +6,13 @@ This module concatenates two arrays
 
 def cat_arrays(arr1, arr2):
     """Function that do the work"""
-    new_arr = arr1
+    new_arr = list(arr1)
     for idx_arr in range(len(arr2)):
         new_arr.append(arr2[idx_arr])
     return new_arr
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [6, 7, 8]
+print(cat_arrays(arr1, arr2))
+print(arr1)
+print(arr2)
