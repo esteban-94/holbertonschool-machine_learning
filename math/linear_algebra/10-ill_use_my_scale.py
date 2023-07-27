5620ceae7434ac1bbb3bdb45fa169c5cc3777589
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-07-26 23:31:36 -0500

    task 10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module return a shape of a matrix
+"""
+
+
+import numpy as np
+
+
+def np_shape(matrix):
+    """Function that do the work"""
+    return matrix.shape
