ee04ca4bb2f56b3b8042060f450a0c556b0ab32c
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-07-27 08:37:45 -0500

    task 12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -7,5 +7,4 @@ and division of two matrices
 
 def np_elementwise(mat1, mat2):
     """Function that do the work"""
-    return (mat1.add(mat1, mat2),mat1.subtract(mat1, mat2)
-            ,mat1.multiply(mat1, mat2),mat1.divide(mat1, mat2))
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
