666708b24f9c9c1e47ddf05d41ff56511742a160
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-07-26 19:27:48 -0500

    fix style

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -10,9 +10,3 @@ def cat_arrays(arr1, arr2):
     for idx_arr in range(len(arr2)):
         new_arr.append(arr2[idx_arr])
     return new_arr
-
-arr1 = [1, 2, 3, 4, 5]
-arr2 = [6, 7, 8]
-print(cat_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
