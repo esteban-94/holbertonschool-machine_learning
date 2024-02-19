c183a48bd69b0f70862e08ed4429a2d44bfe6098
Esteban <juan.esteban.94@outlook.com>
2024-02-18 21:41:14 -0500

    task 0

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -1,39 +1,58 @@
 #!/usr/bin/env python3
-"""0-inception_block.py
-builds an inception block as described
-in Going Deeper with Convolutions (2014)
-"""
+"""Inception Block"""
+
 import tensorflow.keras as K
 
-
 def inception_block(A_prev, filters):
-    """Function that builds an inception block"""
+    """Write a function def inception_block(A_prev, filters):
+    that builds an inception block as described in Going Deeper with
+    Convolutions (2014):
+
+        A_prev is the output from the previous layer
+
+        filters is a tuple or list containing F1, F3R, F3,F5R, F5, FPP,
+        respectively:
+
+            *F1 is the number of filters in the 1x1 convolution
+            *F3R is the number of filters in the 1x1 convolution
+            before the 3x3 convolution
+            *F3 is the number of filters in the 3x3 convolution
+            *F5R is the number of filters in the 1x1 convolution
+            before the 5x5 convolution
+            *F5 is the number of filters in the 5x5 convolution
+            *FPP is the number of filters in the 1x1 convolution
+            after the max pooling
+
+        All convolutions inside the inception block should use a
+        rectified linear activation (ReLU)
+
+        Returns: the concatenated output of the inception block
+"""
     F1, F3R, F3, F5R, F5, FPP = filters
 
-    # 1x1 convolution
-    conv1x1 = K.layers.Conv2D(
-        F1, (1, 1), padding='same', activation='relu')(A_prev)
+    # 1x1 convolution branch with constant filter F1
+    conv1x1 = K.layers.Conv2D(filters=F1, kernel_size=(1, 1),
+                              padding='same', activation='relu')(A_prev)
 
-    # 1x1 convolution before the 3x3 convolution
-    conv3x3_reduce = K.layers.Conv2D(
-        F3R, (1, 1), padding='same', activation='relu')(A_prev)
-    conv3x3 = K.layers.Conv2D(
-        F3, (3, 3), padding='same', activation='relu')(conv3x3_reduce)
+    # Rest of the code remains the same
+    conv3x3_reduce = K.layers.Conv2D(filters=F3R, kernel_size=(1, 1),
+                                     padding='same', activation='relu')(A_prev)
+    conv3x3 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3),
+                              padding='same',
+                              activation='relu')(conv3x3_reduce)
 
-    # 1x1 convolution before the 5x5 convolution
-    conv5x5_reduce = K.layers.Conv2D(
-        F5R, (1, 1), padding='same', activation='relu')(A_prev)
-    conv5x5 = K.layers.Conv2D(
-        F5, (5, 5), padding='same', activation='relu')(conv5x5_reduce)
+    conv5x5_reduce = K.layers.Conv2D(filters=F5R, kernel_size=(1, 1),
+                                     padding='same', activation='relu')(A_prev)
+    conv5x5 = K.layers.Conv2D(filters=F5, kernel_size=(5, 5),
+                              padding='same',
+                              activation='relu')(conv5x5_reduce)
 
-    # Max pooling and 1x1 convolution after the max pooling
-    max_pool = K.layers.MaxPooling2D(
-        (3, 3), strides=(1, 1), padding='same')(A_prev)
-    conv_pool = K.layers.Conv2D(
-        FPP, (1, 1), padding='same', activation='relu')(max_pool)
+    pool = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(1, 1),
+                                 padding='same')(A_prev)
+    pool_proj = K.layers.Conv2D(filters=FPP, kernel_size=(1, 1),
+                                padding='same', activation='relu')(pool)
 
-    # Concatenate the outputs
-    inception_output = K.layers.concatenate(
-        [conv1x1, conv3x3, conv5x5, conv_pool], axis=-1)
+    concat = K.layers.concatenate(
+        [conv1x1, conv3x3, conv5x5, pool_proj], axis=-1)
 
-    return inception_output
+    return concat
