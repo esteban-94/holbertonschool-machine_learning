7b64af60e4a9d8e3b387d7c96b0740c5c59a2951
Esteban <juan.esteban.94@outlook.com>
2024-02-27 19:35:43 -0500

    task 2

---
+++ supervised_learning/object_detection/2-yolo.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+""" Yolo v3 algorithm to perform object detectio """
+import tensorflow.keras as K
+import numpy as np
+
+
+class Yolo:
+    """
+        Class Yolo that uses the Yolo v3 algorithm to
+        perform object detection
+    """
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """ Class Constructor. """
+        self.model = K.models.load_model(model_path)
+        with open(classes_path, 'r') as f:
+            self.class_names = [line.strip() for line in f]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def sigmoid(self, x):
+        """ Sigmoid function """
+        return (1 / (1 + np.exp(-x)))
+
+    def process_outputs(self, outputs, image_size):
+        """ Process the outputs of the class """
+        boxes = [pred[:, :, :, 0:4] for pred in outputs]
+        for ipred, pred in enumerate(boxes):
+            for grid_h in range(pred.shape[0]):
+                for grid_w in range(pred.shape[1]):
+                    bx = ((self.sigmoid(pred[grid_h, grid_w, :, 0]) + grid_w)
+                          / pred.shape[1])
+                    by = ((self.sigmoid(pred[grid_h, grid_w, :, 1]) + grid_h)
+                          / pred.shape[0])
+                    anchor_tensor = self.anchors[ipred].astype(float)
+                    anchor_tensor[:, 0] *= np.exp(pred[grid_h, grid_w, :, 2])\
+                        / self.model.input.shape[1].value
+                    anchor_tensor[:, 1] *= np.exp(pred[grid_h, grid_w, :, 3])\
+                        / self.model.input.shape[2].value
+                    pred[grid_h, grid_w, :, 0] = (bx - (anchor_tensor[:, 0] /
+                                                  2)) * image_size[1]
+                    pred[grid_h, grid_w, :, 1] = (by - (anchor_tensor[:, 1] /
+                                                  2)) * image_size[0]
+                    pred[grid_h, grid_w, :, 2] = (bx + (anchor_tensor[:, 0] /
+                                                  2)) * image_size[1]
+                    pred[grid_h, grid_w, :, 3] = (by + (anchor_tensor[:, 1] /
+                                                  2)) * image_size[0]
+        box_confidences = [self.sigmoid(pred[:, :, :, 4:5]) for pred in
+                           outputs]
+        box_class_probs = [self.sigmoid(pred[:, :, :, 5:]) for pred in outputs]
+        return (boxes, box_confidences, box_class_probs)
+
+    def filter_boxes(self, boxes, box_confidences, box_class_probs):
+        """ Filter boxes """
+        box_score = []
+        bc = box_confidences
+        bcp = box_class_probs
+        for box_conf, box_probs in zip(bc, bcp):
+            score = (box_conf * box_probs)
+            box_score.append(score)
+        box_classes = [s.argmax(axis=-1) for s in box_score]
+        box_class_l = [b.reshape(-1) for b in box_classes]
+        box_classes = np.concatenate(box_class_l)
+        box_class_scores = [s.max(axis=-1) for s in box_score]
+        b_scores_l = [b.reshape(-1) for b in box_class_scores]
+        box_class_scores = np.concatenate(b_scores_l)
+        mask = np.where(box_class_scores >= self.class_t)
+        boxes_all = [b.reshape(-1, 4) for b in boxes]
+        boxes_all = np.concatenate(boxes_all)
+        return (boxes_all[mask], box_classes[mask], box_class_scores[mask])
