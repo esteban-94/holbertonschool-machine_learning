a040c42bc803bcce74c4e820392dd4aaadf90a24
Esteban <juan.esteban.94@outlook.com>
2024-04-07 22:36:01 -0500

    task 11

---
+++ unsupervised_learning/clustering/11-gmm.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Module gmm"""
+
+import sklearn.mixture
+
+
+def gmm(X, k):
+    """
+      Calculate a GMM from a dataset.
+    """
+    g = sklearn.mixture.GaussianMixture(n_components=k)
+    g.fit(X)
+    pi = g.weights_
+    m = g.means_
+    S = g.covariances_
+    clss = g.predict(X)
+    bic = g.bic(X)
+    return pi, m, S, clss, bic
---
+++ unsupervised_learning/clustering/2-variance.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Module: Variance """
+
+import numpy as np
+
+
+def variance(X, C):
+    """
+      Calculate the total intra-cluster variance.
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(C, np.ndarray) or len(C.shape) != 2:
+        return None
+    if X.shape[1] != C.shape[1]:
+        return None
+    n, d = X.shape
+    centroids_extended = C[:, np.newaxis]
+    distances = np.sqrt(((X - centroids_extended) ** 2).sum(axis=2))
+    min_distances = np.min(distances, axis=0)
+    variance = np.sum(min_distances ** 2)
+    return variance
