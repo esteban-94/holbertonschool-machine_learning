2d484ccfbfb87cdd7539a08485b99640685b59b7
Esteban <juan.esteban.94@outlook.com>
2024-04-14 20:54:48 -0500

    task 1

---
+++ unsupervised_learning/hmm/1-regular.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module Regular"""
+
+import numpy as np
+
+
+def regular(P):
+    """
+      Determine the steady state probabilities
+      of a regular markov chain.
+    """
+    try:
+        if len(P.shape) != 2:
+            return None
+        n = P.shape[0]
+        if n != P.shape[1]:
+            return None
+        evals, evecs = np.linalg.eig(P.T)
+        state = (evecs / evecs.sum())
+        new_state = np.dot(state.T, P)
+        for i in new_state:
+            if (i >= 0).all() and np.isclose(i.sum(), 1):
+                return i.reshape(1, n)
+    except Exception:
+        return None
